98aa238197c31000d0695376780bcb532a5d861b
Kyle Tulod <tulod.kyle.m@gmail.com>
2017-02-14 14:42:52 -0500

    sends post to create user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,24 @@
+import axios from 'axios'
+import { browserHistory } from 'react-router'
 
-export function createUser(user) {
+const URL = "http://localhost:3000/api/v1/"
 
-  return { type: 'CREATE_USER',
-           payload: 'test'
+export const createUser = (user) => {
+  let response = axios.post( URL + 'signup', user).then ((innerResponse) => {
+    sessionStorage.setItem('jwt', innerResponse.data.jwt)
+    // browserHistory.push("/drinks")
+    return innerResponse
+  })
+
+  return {
+    type: "SIGN_UP",
+    payload: response
   }
 }
+
+// export function createUser(user) {
+//
+//   return { type: 'CREATE_USER',
+//            payload: 'test'
+//   }
+// }
