21d5731884a843012d8b02c6a9bc641d16c3551b
Jenny <jenny.kats@aol.com>
2017-02-15 15:48:30 -0500

    login auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,19 @@ import { browserHistory } from 'react-router'
 import  usersAdapter from '../adapters/usersAdapter'
 
 export function createUser(userParams){
-  const user = usersAdapter.loginUser(userParams)
+  const user = usersAdapter.createUser(userParams)
 
     return {
       payload: user,
       type: 'SIGN_UP'
     }
   }
+
+export function loginUser(userParams) {
+  const user = usersAdapter.loginUser(userParams)
+
+  return {
+    payload: user,
+    type: "LOG_IN"
+  }
+}
