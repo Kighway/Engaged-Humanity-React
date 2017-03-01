72cdf5fdcbc7de75fc1834831191de07006c721a
Jenny <jenny.kats@aol.com>
2017-03-01 12:17:59 -0500

    changed base url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
 
-  base_url: 'http://localhost:3000/api/v1'
-//  base_url: 'https://collective-news-curator-api.herokuapp.com/api/v1'
+  // base_url: 'http://localhost:3000/api/v1'
+ base_url: 'https://collective-news-curator-api.herokuapp.com/api/v1'
 
   }
